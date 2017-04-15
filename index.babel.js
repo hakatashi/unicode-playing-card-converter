9064f7c93575bc546193014dd14cc6ffa05d5f47
Koki Takahashi <hakatasiloving@gmail.com>
2017-04-15 16:51:10 +0900

    Add first code

---
+++ index.babel.js
@@ -0,0 +1,9 @@
+// @flow
+
+module.exports.fromCard = (card: {suit: string, rank: number}) => {
+  throw new Error('not implemented');
+};
+
+module.exports.fromSpecials = (card: {type: string, variation: number}) => {
+  throw new Error('not implemented');
+};
